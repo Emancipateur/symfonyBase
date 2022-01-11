f4fff3c89d1b97f8cbabc01f3dda96bd253e9485
Kevin Cenerino <cenerinok@gmail.com>
2022-01-11 09:15:05 +0100

    form&repository

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Entity\Articles;
 use App\Entity\Category;
 use App\Repository\ArticlesRepository;
 use App\Repository\CategoryRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class DefaultController extends AbstractController
@@ -102,7 +104,6 @@ class DefaultController extends AbstractController
 
         foreach ($articles as $article){
         $article->getCategory()->getTitle();
-        dump($article);
     
         }
         return $this->render('default/showArticles.html.twig',["articles" => $articles]);
@@ -143,7 +144,27 @@ class DefaultController extends AbstractController
    }
 
 
-}
 
+
+/**
+ *@Route("/userForm")
+ */
+public function new(Request $request): Response
+{
+    $user = new User();
+
+    $form = $this->createForm(UserType::class, $user);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        dd($user);
+    }
+
+ return $this->render('default/user.html.twig',["form" => $form->createView()]);
+
+
+}
+}
 
 
---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('email', EmailType::class)
+            ->add('birthdate', DateType::class)
+        ;
+    }
+}
